5ff23cf9216bfc943c001cd013beae1be4d594b2
Remcycling <22045554@myrp.edu.sg>
2023-05-04 12:06:06 +0800

    Completed up to section D

--- app/src/main/java/sg/edu/rp/c346/id22045554/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22045554/week03/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+
+    ToggleButton tbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
 
 
 
@@ -35,6 +39,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add your code for the action
+                //get state of toggle button
+                boolean isChecked = tbtn.isChecked();
+
+                //-	disable the EditText, editTextInput when the toggle button is unchecked
+                //-	enable it when it is checked
+
+                if(isChecked){
+                    etInput.setEnabled(true);
+                }
+                else {
+                    etInput.setEnabled(false);
+                }
+
+            }
+        });
+
 
     }
 }
