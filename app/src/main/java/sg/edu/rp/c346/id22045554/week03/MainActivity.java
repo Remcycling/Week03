42ac73e46ebcde0953c80faeffb7df6a17924031
Remcycling <22045554@myrp.edu.sg>
2023-05-04 11:25:42 +0800

    Completed up to section C

--- app/src/main/java/sg/edu/rp/c346/id22045554/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22045554/week03/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id22045554.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+
+
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+            }
+        });
+
     }
 }
